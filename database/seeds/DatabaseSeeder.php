5426fb0eebc7055509801f6ccc2f9937c921a3d0
Julien1138 <julien.aupart@gmail.com>
2017-11-02 12:16:13 +0100

    Create article table in database + seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Article;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //suppression du contenu des tables
+        DB::table('articles')->delete();
+
+        $fakeUser = User::create(array(
+            'name'      => 'FakeUser',
+            'email'     => 'fake@user.com',
+            'password'  => bcrypt('secret')
+        ));
+
+        $article1 = Article::create(array(
+            'title'     => 'Premier article',
+            'content'   => 'Un contenu pour ce permier article',
+        ));
     }
 }
